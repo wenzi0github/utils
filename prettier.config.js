34298f3444f7b084a17fce230e319270522f86ab
skeetershi(师少兵) <skeetershi@tencent.com>
2020-05-27 12:16:42 +0800

    chore(prettier): 添加prettier配置

---
+++ prettier.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    // tab缩进大小,默认为2
+    tabWidth: 4,
+    // 使用tab缩进，默认false
+    // useTabs: true,
+    // 使用分号, 默认true
+    semi: true,
+    // 使用单引号, 默认false(在jsx中配置无效, 默认都是双引号)
+    singleQuote: false,
+    // 行尾逗号,默认none,可选 none|es5|all
+    // es5 包括es5中的数组、对象
+    // all 包括函数对象等所有可选
+    TrailingCooma: "none",
+    // 对象中的空格 默认true
+    // true: { foo: bar }
+    // false: {foo: bar}
+    bracketSpacing: true,
+    // JSX标签闭合位置 默认false
+    // false: <div
+    //          className=""
+    //          style={{}}
+    //       >
+    // true: <div
+    //          className=""
+    //          style={{}} >
+    arrowParens: "always",
+};
