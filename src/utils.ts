8602fb04378952c9ee18e1927634ebe0b8a2d48d
skeetershi(师少兵) <skeetershi@tencent.com>
2020-06-08 10:11:46 +0800

    feat(utils): add export default home

---
+++ src/utils.ts
@@ -0,0 +1,11 @@
+import * as cookie from "./cookie";
+import * as date from "./date";
+import * as debounceThrottle from "./debounce-throttle";
+import * as querystring from "./querystring";
+import * as regexp from "./regexp";
+import * as string from "./string";
+import * as ua from "./ua";
+import * as url from "./url";
+import * as visibility from "./visibility";
+
+export { cookie, date, debounceThrottle, querystring, regexp, string, ua, url, visibility };
