8ab0371798c2cd971f74dc381e25489ab686a9df
skeetershi(师少兵) <skeetershi@tencent.com>
2020-06-14 00:54:14 +0800

    feat(ua): add android model

--- src/ua.ts
+++ src/ua.ts
@@ -2,20 +2,23 @@
 // https://github.com/fynas/ua/blob/master/ua_string.csv
 // https://github.com/KHwang9883/MobileModels
 
-const userAgent = window.navigator.userAgent.toLocaleLowerCase();
-
 /**
  * 获取当前系统和版本号
  */
-export const getSystemInfo = () => {
+export const getSystemInfo = (uagt?: string) => {
     const os = {
         name: "",
         version: "",
         ios: false,
         android: false,
         manufacture: "", // 手机品牌
-        model: "" // 品牌中的型号
+        model: "", // 品牌中的型号
+        build: "" // 生产流水线，一般与型号对应，但型号对用户来说更加直观。例如小米2s中，品牌为xiaomi，型号为2s，流水线为JRO03L
     };
+    if (typeof window === "undefined") {
+        return os;
+    }
+    const userAgent = (uagt || window.navigator.userAgent).toLocaleLowerCase();
     const osTypes = {
         iphone: userAgent.match(/(iphone)\s(os\s)?([\d_]+)/i),
         ipad: userAgent.match(/(ipad).*\s([\d_]+)/i),
@@ -40,7 +43,8 @@ export const getSystemInfo = () => {
             vivo: /vivo/.test(userAgent), // vivo手机
             xiaomi: /hm|mi/i.test(userAgent), // 小米手机
             redmi: /redmi/.test(userAgent), // 红米手机
-            oppo: /oppo/.test(userAgent) // OPPO
+            oppo: /oppo/.test(userAgent), // OPPO
+            oneplus: /oneplus/.test(userAgent) // 一加手机
         };
         // 匹配机型
         for (let key in androidTypes) {
@@ -49,10 +53,15 @@ export const getSystemInfo = () => {
                 break;
             }
         }
-        // 匹配品牌
-        const modelType = userAgent.match(new RegExp(`build/(${os.manufacture})?(.*?)[()|;)$]`));
-        if (modelType && modelType.length >= 3) {
-            os.model = modelType[2];
+        // 匹配型号
+        const modelType = userAgent.match(/zh-cn;(.*)build/);
+        if (modelType && modelType.length >= 2) {
+            os.model = modelType[1].replace(os.manufacture, "").replace(/(^\s*)|(\s*$)/g, "");
+        }
+        // 匹配流水线
+        const buildType = userAgent.match(new RegExp(`build/(${os.manufacture})?(.*?)[()|;)$]`));
+        if (buildType && buildType.length >= 3) {
+            os.build = buildType[2];
         }
     } else if (osTypes.ipad) {
         os.name = "ipad";
@@ -83,7 +92,7 @@ type BrowserInfoKeys =
 /**
  * 获取当前APP和版本号
  */
-export const getBrowserInfo = () => {
+export const getBrowserInfo = (uagt?: string) => {
     const browser: {
         name: BrowserInfoKeys | "";
         version: string;
@@ -91,6 +100,11 @@ export const getBrowserInfo = () => {
         name: "",
         version: ""
     };
+    if (typeof window === "undefined") {
+        return browser;
+    }
+    const userAgent = (uagt || window.navigator.userAgent).toLocaleLowerCase();
+
     // 腾讯系的APP要先判断是否在广告webview中
     // 之前广平的webview只有他自己的标识，现在会加上其所在APP的标识
     // 若不先判处，则容易被误判在真实的客户端内，导致使用真实APP的jsapi时发生错误
