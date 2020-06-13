d21e8852330cf6892adbd3d8096f38f791f88196
skeetershi(师少兵) <skeetershi@tencent.com>
2020-06-14 01:02:55 +0800

    test(ua): add ua test

--- __tests__/ua.test.ts
+++ __tests__/ua.test.ts
@@ -3,6 +3,10 @@ import { getSystemInfo } from "../src/ua";
 describe("", () => {
     const ua = "Mozilla/5.0 (iPod; CPU iPhone OS 6_0_1 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A523 Safari/8536.25";
     it("ios", () => {
-        console.log(getSystemInfo(ua));
+        const { name, version, ios, android } = getSystemInfo(ua);
+        expect(name).toBe("iphone");
+        expect(version).toBe("6.0.1");
+        expect(ios).toBeTruthy();
+        expect(android).toBeFalsy();
     });
 });
