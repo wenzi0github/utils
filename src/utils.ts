444d4ed396a2f9f8257f3dd0c6731928ecf305db
skeetershi(师少兵) <skeetershi@tencent.com>
2020-07-17 14:05:48 +0800

    chore(package): update version

--- src/utils.ts
+++ src/utils.ts
@@ -2,10 +2,11 @@ import * as cookie from "./cookie";
 import * as date from "./date";
 import * as debounceThrottle from "./debounceThrottle";
 import * as querystring from "./querystring";
+import * as random from "./random";
 import * as regexp from "./regexp";
 import * as string from "./string";
 import * as ua from "./ua";
 import * as url from "./url";
 import * as visibility from "./visibility";
 
-export { cookie, date, debounceThrottle, querystring, regexp, string, ua, url, visibility };
+export { cookie, date, debounceThrottle, querystring, random, regexp, string, ua, url, visibility };
